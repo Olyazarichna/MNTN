7b75da9d351789891d2be65ab0a9804b2b63ea37
Olha Zarichna <olya.zarichna@gmail.com>
2023-07-04 11:29:37 +0300

    Update Footer.jsx

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -42,13 +42,13 @@ const Footer = () => {
                 </a>
               </li>
               <li className={styles.list_item}>
-                <button
+                <a
                   className={styles.list_link}
                   onClick={openModal}
                   href="#contact"
                 >
                   Contact Us
-                </button>
+                </a>
               </li>
               <li className={styles.list_item}>
                 <Link className={styles.list_link} href="/">
